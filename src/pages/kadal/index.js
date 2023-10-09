31dd0be6fcec4c806a1fd3c487bd39a5af476dab
dep01 <57895253+dep01@users.noreply.github.com>
2023-10-09 17:08:40 +0700

    feat(sub menus)

--- src/pages/kadal/index.js
+++
@@ -1,3 +0,0 @@
-import KadalList from "./List";
-  import KadalForm from "./Form";
-  export {KadalForm,KadalList}
--- src/pages/kadal/store/schema_form.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-  export const FORM_SCHEMA = {  
-    
-        time:yup.string().required("Required!"),
-        name:yup.string().required("Required!"),
-        age:yup.number().required("Required!")
-  };
